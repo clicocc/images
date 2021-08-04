bbd1de7fb8e5c8c8e394e5b246ad8cf8f3d25f24
clicocc <52556612+clicocc@users.noreply.github.com>
2021-08-04 08:48:27 +0800

    Upload1

---
+++ images/2021/08/04/tool.js
@@ -0,0 +1,8 @@
+$('a').tooltip({trigger:'click',placement:'bottom'});function setTooltip(btn,message){$(btn).tooltip('hide').attr('data-original-title',message).tooltip('show');}
+function hideTooltip(btn){setTimeout(function(){$(btn).tooltip('hide');},1000);}
+var clipboard=new Clipboard('a');clipboard.on('success',function(e){setTooltip(e.trigger,'Copied to clipboard!');hideTooltip(e.trigger);});clipboard.on('error',function(e){setTooltip(e.trigger,'Failed!');hideTooltip(e.trigger);});$('a').tooltip({trigger:'click',placement:'bottom'});function setTooltip(btn1,message){$(btn1).tooltip('hide').attr('data-original-title',message).tooltip('show');}
+function hideTooltip(btn1){setTimeout(function(){},1000);}
+var clipboard1=new Clipboard('span');clipboard1.on('success',function(e){setTooltip(e.trigger,'Copied to clipboard!');hideTooltip(e.trigger);});clipboard1.on('error',function(e){setTooltip(e.trigger,'Failed!');hideTooltip(e.trigger);})
+$('a').tooltip({trigger:'click',placement:'bottom'});function setTooltip(btn2,message){$(btn2).tooltip('hide').attr('data-original-title',message).tooltip('show');}
+function hideTooltip(btn2){setTimeout(function(){},1000);}
+var clipboard2=new Clipboard('h1');clipboard2.on('success',function(e){setTooltip(e.trigger,'Copied to clipboard!');hideTooltip(e.trigger);});clipboard2.on('error',function(e){setTooltip(e.trigger,'Failed!');hideTooltip(e.trigger);});var upClick=function(){var rurl=$("link[rel=canonical]").attr("href");console.log(rurl);window.location=rurl;}
